e0edd97acf43ed6be155eb440b36ff199039d686
AllanMuhari <allanmuhari087@gmail.com>
2023-08-05 14:18:47 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import logo from '../../Assets/GloboLogo.png'
 const Header = () => {
   return (
     <div>
-      <div className='w-px h-2'>
-        <img src={logo} alt='globo logo'/>
-      </div>
-       <div>
-        <h1 className='text-2xl font-bold'>Globo News</h1>
+      <div className='flex flex-row '>
+        <img className='h-36' src={logo} alt='globo logo'/>
+        <div>
+        <h1 className='text-xl italic #c2410c float-right '>Providing houses all over the world</h1> 
        </div>
+      </div>
     </div>
   )
 }
